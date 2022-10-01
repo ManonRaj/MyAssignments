380e841a93c0f52fad141b4365902717889c08cb
ManonRaj <114816368+ManonRaj@users.noreply.github.com>
2022-10-01 19:26:36 +0530

    Add files via upload

---
+++ day1/Car.java
@@ -0,0 +1,25 @@
+package week1.day1;
+
+public class Car {
+
+	public static void main(String[] args) {
+		
+		//Primitive Data types
+		
+		byte byteValue = 127;
+		short shortValue = 32767;
+		int intValue = 2022;
+		Long longValue = 20220626L;
+		float floatValue = 30.4f;
+		double doubleValue = 3490.93847;
+		char letter = 'M';
+		boolean hisnameismanon = true;
+		
+		//Non-Primitive Data types
+		
+		String name = "Manon";
+		
+		
+	}
+
+}
