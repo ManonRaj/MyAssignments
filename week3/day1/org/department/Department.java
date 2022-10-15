53fb351573b813991d33a007b88f7825fc44004a
ManonRaj <114816368+ManonRaj@users.noreply.github.com>
2022-10-15 23:10:40 +0530

    Add files via upload

---
+++ week3/day1/org/department/Department.java
@@ -0,0 +1,11 @@
+package week3.day1.org.department;
+
+import week3.day1.org.college.College;
+
+public class Department extends College {
+	public void deptName() {
+		String deptName = "Computer Sceince";
+		System.out.println("Department Name is :" + deptName);
+	}
+
+}
